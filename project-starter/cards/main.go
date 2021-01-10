a283fb05f35ede905c854b3d000fd3db8a60080d
Nugraha.Pradana <nugraha.pradana@btpn.com>
2021-01-10 10:51:34 +0700

    [Adit] implement array and loop in golang

--- project-starter/cards/main.go
+++ project-starter/cards/main.go
@@ -6,6 +6,15 @@ func main() {
 	printState()
 	card := createNewCard()
 	fmt.Println(card)
+
+	cards := []string{"Five of Spade", createNewCard()}
+	cards = append(cards, "Jack of Diamond")
+	fmt.Println(cards)
+	fmt.Println(len(cards))
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func createNewCard() string {
